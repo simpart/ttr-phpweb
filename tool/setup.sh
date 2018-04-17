c9f279bfbb0899533e98d130caae2ba2b8f3d0d9
simpart <simpart>
2018-04-17 14:40:55 +0900

    add error catch

--- tool/setup.sh
+++ tool/setup.sh
@@ -3,6 +3,17 @@ SCP_DIR=$(cd $(dirname $0);pwd);
 cd $SCP_DIR
 
 bash ./setup/develop.sh
+if [ $? != 0 ]; then
+    exit -1
+fi
+
 bash ./setup/php.sh
-bash ./setup/http.sh
+if [ $? != 0 ]; then
+    exit -1
+fi
+
+bash ./setup/httpd.sh
+if [ $? != 0 ]; then
+    exit -1
+fi
 

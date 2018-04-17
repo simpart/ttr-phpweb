ba4f34928e63e92ae8ac07be66169deea37578ea
simpart <simpart>
2018-04-17 11:14:33 +0900

    enable command line paramter

--- tool/build.sh
+++ tool/build.sh
@@ -1,27 +1,33 @@
 #!/bin/bash 
 SCP_DIR=$(cd $(dirname $0);pwd);
-TGT_PATH=''
-APP_TITLE=''
+TGT_PATH=$1
+APP_TITLE=$2
 
 get_inf () {
     echo "start ttr-web build"
     cd $SCP_DIR/../
-    echo -n "deploy path : "
-    read TGT_PATH
-    
+
+    # get deploy target path
+    if [[ $TGT_PATH == "" ]]; then
+        echo -n "deploy path : "
+        read TGT_PATH
+    fi
     if [ ! -d $TGT_PATH ]; then
         error "could not found target path"
     fi
     
-    echo -n "app title : "
-    read APP_TITLE
-    if [[ "" == ${APP_TITLE} ]]; then
+    # get application title
+    if [[ $APP_TITLE == "" ]]; then
+        echo -n "app title : "
+        read APP_TITLE
+    fi
+    if [[ $APP_TITLE == "" ]]; then
         error "app title is null"
     fi
     
-    if [ ! -d $TGT_PATH ]; then
+    if [ ! -d $TGT_PATH/$APP_TITLE ]; then
         echo "*** create app directory"
-        mkdir $TGT_PATH/$APP_TITLE
+        sudo mkdir $TGT_PATH/$APP_TITLE
         if [ $? != 0 ]; then
             error "could not create $TGT_PATH/$APP_TITLE directory"
         fi

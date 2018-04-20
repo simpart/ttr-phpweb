b9f6f11592b6b8c9f12ef59d0f9083b781a836bc
simpart <simpart>
2018-04-20 16:44:32 +0900

    mod user

--- src/php/usr/User.php
+++ src/php/usr/User.php
@@ -4,19 +4,20 @@
  * @brief user common class
  */
 namespace usr;
+require_once(__DIR__ . '/../com/define.php');
 require_once(__DIR__ . '/define.php');
 
-class User {
-    private $id     = null;
-    private $name   = null;
-    private $passwd = null;
-    private $role   = null;
+class User extends \ttr\db\mongo\Document {
     
-    function __construct ($nm=null, $pwd=null, $rol=null) {
+    function __construct ($nm=null, $pwd=null, $rle=null) {
         try {
+            parent::__construct(
+                array('name', 'password', 'role'),
+                array('name', 'password', 'role')
+            );
             $this->name($nm);
             $this->password($pwd);
-            $this->role($rol);
+            $this->role($rle);
         } catch (\Exception $e) {
             throw new \Exception(
                 PHP_EOL   .
@@ -29,43 +30,19 @@ class User {
         }
     }
    
-    public function id ($prm=null) {
-        try {
-            if (null === $prm) {
-                /* getter */
-                if (null === $this->id) {
-                     $this->id(uuid_create(UUID_TYPE_RANDOM));
-                }
-                return $this->id;
-            }
-            /* setter */
-            if ('string' !== gettype($prm)) {
-                throw new \Exception('invalid data type');
-            }
-            $this->id = $prm;
-        } catch (\Exception $e) {
-            throw new \Exception(
-                PHP_EOL   .
-                'File:'   . __FILE__         . ',' .
-                'Line:'   . __line__         . ',' .
-                'Class:'  . get_class($this) . ',' .
-                'Method:' . __FUNCTION__     . ',' .
-                $e->getMessage()
-            );
-        }
-    }
-    
     public function name ($prm=null) {
         try {
             if (null === $prm) {
                 /* getter */
-                return $this->name;
+                return $this->contents()['name'];
             }
             /* setter */
             if ('string' !== gettype($prm)) {
                 throw new \Exception('invalid data type');
             }
-            $this->name = $prm;
+            $this->contents(
+                array('name' => $prm)
+            );
         } catch (\Exception $e) {
             throw new \Exception(
                 PHP_EOL   .
@@ -83,21 +60,21 @@ class User {
         try {
             if (null === $prm) {
                 /* getter */
-                return $this->passwd;
+                return $this->contents()['password'];
             }
             /* setter */
             if ('string' !== gettype($prm)) {
                 throw new \Exception('invalid data type');
             }
-            if (true === $md5) {
-                /**
-                 * @attention this password hash is so weak
-                 *            this is workaround
-                 */
-                $this->passwd = md5($prm);
-            } else {
-                $this->passwd = $prm;
-            }
+            
+            /**
+             * @attention this password hash is so weak
+             *            this is workaround
+             */
+            $set_pwd = (true === $md5) ? md5($prm) : $prm;
+            $this->contents(
+                array('password' => $set_pwd)
+            );
         } catch (\Exception $e) {
             throw new \Exception(
                 PHP_EOL   .
@@ -114,44 +91,16 @@ class User {
         try {
             if (null === $prm) {
                 /* getter */
-                return $this->role;
+                return $this->contents()['role'];
             }
             /* setter */
             if ( (DUSR_ROLE_ADMIN !== $prm) &&
                  (DUSR_ROLE_USER !== $prm) ) {
                 throw new \Exception('invalid parameter');
             }
-            $this->role = $prm;
-        } catch (\Exception $e) {
-            throw new \Exception(
-                PHP_EOL   .
-                'File:'   . __FILE__         . ',' .
-                'Line:'   . __line__         . ',' .
-                'Class:'  . get_class($this) . ',' .
-                'Method:' . __FUNCTION__     . ',' .
-                $e->getMessage()
+            $this->contents(
+                array('role' => $prm)
             );
-        }
-    }
-    
-    public function isSetted ($chk) {
-        try {
-            if ( (DUSR_ELM_ID & $chk) && (null === $this->id()) ) {
-                return false;
-            }
-            
-            if ( (DUSR_ELM_NAME & $chk) && (null === $this->name()) ) {
-                return false;
-            }
-            
-            if ( (DUSR_ELM_PWD & $chk) && (null === $this->password()) ) {
-                return false;
-            }
-            
-            if ( (DUSR_ELM_ROL & $chk) && (null === $this->role()) ) {
-                return false;
-            }
-            return true;
         } catch (\Exception $e) {
             throw new \Exception(
                 PHP_EOL   .
